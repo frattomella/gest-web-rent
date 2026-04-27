45ca778bc174b2a631211f05588af1f43a22baf8
frattomella <mefrancesco2007y@gmail.com>
2026-04-27 17:06:33 +0200

    Fix catalog block registration attributes

--- public/assets/js/gwr-catalog-block.js
+++ public/assets/js/gwr-catalog-block.js
@@ -1,20 +1,40 @@
-(function (blocks, element, components, blockEditor, serverSideRender) {
+(function (blocks, element, serverSideRender) {
     var el = element.createElement;
 
     blocks.registerBlockType('gest-web-rent/catalog', {
+        apiVersion: 2,
         title: 'Gest Web Rent Catalogo',
+        description: 'Catalogo veicoli a noleggio con filtri date, card e dettaglio in overlay.',
         icon: 'car',
         category: 'widgets',
+        supports: {
+            html: false,
+            align: ['wide', 'full']
+        },
+        attributes: {
+            title: {
+                type: 'string',
+                default: 'Catalogo noleggio'
+            },
+            subtitle: {
+                type: 'string',
+                default: 'Scegli le date, filtra il parco veicoli e apri i dettagli senza cambiare pagina.'
+            },
+            max_width: {
+                type: 'string',
+                default: '1380'
+            }
+        },
 
         edit: function (props) {
             return el(serverSideRender, {
                 block: 'gest-web-rent/catalog',
-                attributes: props.attributes
+                attributes: props.attributes || {}
             });
         },
 
         save: function () {
-            return null; // dynamic block
+            return null;
         }
     });
-})(window.wp.blocks, window.wp.element, window.wp.components, window.wp.blockEditor, window.wp.serverSideRender);
+})(window.wp.blocks, window.wp.element, window.wp.serverSideRender);
